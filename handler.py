06d97343796e4d61c24431acb236d9c272a94d1f
Fernando Medina Corey <fmcorey@gmail.com>
2020-01-08 09:06:53 -0800

    Add requests dependency

--- handler.py
+++ handler.py
@@ -1,24 +1,14 @@
 import json
 
+import requests
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
+    print(event)
+    result = requests.get('https://fernandomc.com').text
+    print(result)
+    body = {"result": result}
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
-
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
